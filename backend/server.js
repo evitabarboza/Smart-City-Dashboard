e65b6f081b0ee015b14bc017767ca38d60a9c6d1
aaron21032004 <aaronfernandescks2015@gmail.com>
2025-02-06 10:34:58 +0530

    Sever initialized

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Smart City Dashboard Backend is Running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
